b012d71dfbc2240fe84edcc7e294d7f35b741c71
Bikash Kumar Sharma <bikash1999@yahoo.com>
2014-08-25 15:36:24 +0530

    changed src to url

--- js/ngPdfObject.js
+++ js/ngPdfObject.js
@@ -12,14 +12,14 @@
 		return {
 			replace: 'true',
 			scope:{
-				src : "@src",
+				url : "@url",
 				height: "@height",
 				width: "@width"
 			},
 			template: '<div id="pdf"><div>',			
 			link: function (scope, elem, attrs) {
 				
-				var success = new PDFObject({ url: scope.src, width:scope.height, height:scope.height }).embed("pdf");				
+				var success = new PDFObject({ url: scope.url, width:scope.height, height:scope.height }).embed("pdf");				
 			}
 		};
 	});
